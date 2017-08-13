8093ef6ff0015ac5f39dab66e035ddc66a5ec9bd
GeorgiAtanasov87 <georgiatanasov87@gmail.com>
2017-08-13 14:34:08 +0300

    Update readme.txt

---
+++ automateTheBoringStuff/08-Files/madlibs.py
@@ -0,0 +1,16 @@
+#! python3
+# madlibs.py - script replacing words 'ADJECTIVE', 'NOUN', 'VERB'
+
+import re
+
+# TODO: open the file
+sfile = open('sample_text.txt', r)
+for line in sfile:
+    print line
+# TODO: search for each word
+
+# TODO: prompt for replacement
+
+# TODO: print the text
+
+# TODO: save to new file
